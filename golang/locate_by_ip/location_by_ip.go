14426999dfe25cb05fb33bbbb2b478f04da93fde
pwhite <pwhite00@aol.com>
2017-06-22 20:18:25 -0700

    more formatting for lcoation_by_ip

--- golang/locate_by_ip/location_by_ip.go
+++ golang/locate_by_ip/location_by_ip.go
@@ -94,8 +94,10 @@ func call_api(url string) {
 	}
 
 	// write the json output. normally in a more interesting way.
+	fmt.Println(time.Now())
 	fmt.Println("Ipaddress", details1.Ipaddress)
 	fmt.Println(details1.City, ",", details1.Region_name, ",", details1.Country_name)
+	fmt.Println("Zip code:", details1.Zip_code, "Time Zone:", details1.Time_zone, "Metro_Code:", details1.Metro_Code)
 	fmt.Println("Latitude:", details1.Latitude, "Longitude:", details1.Longitude)
 
 }
